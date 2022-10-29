21f5bd246f927d8dd7487f455588301246c5323b
Florencia <florencia@thehit.com>
2022-10-29 12:39:34 -0300

    feature: agrega ejemplo fetch api y scroll infiito

--- react-native/react-navigation-example/screens/NotificationsScreen.js
+++ react-native/react-navigation-example/screens/NotificationsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,16 +7,15 @@ import {
   TouchableOpacity,
   FlatList,
   ActivityIndicator,
-  Platform,
+  Platform
 } from "react-native";
-import notifications from "../services/notifications";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 //////////////////
 // Con FlatList //
 //////////////////
 const NotificationItem = ({
-  notification,
+  user,
   backgroundColor,
   textColor,
   iconColor,
@@ -33,7 +32,7 @@ const NotificationItem = ({
         color={iconColor}
       />
       <Text style={[styles.notificationText, textColor]}>
-        {notification.message}
+        {user} ha comenzado a seguirte
       </Text>
     </TouchableOpacity>
   );
@@ -41,39 +40,52 @@ const NotificationItem = ({
 
 export default function NotificationsScreen() {
   const [selectedItemId, setSelectedItemId] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [users, setUsers] = useState([]);
+  const API_HOST = "https://randomuser.me/api"; 
+  
+  // Cada vez que la variable currentPage cambie, se va a ejecutar la función getUsers()
+  useEffect(() => {
+    getUsers();
+ }, [currentPage]);
+
+
+ // Cada vez que llega al final de la lista va a cambiar el número de página
+  const loadMoreItems =  async () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  // Va a obtener la data de los usuarios (para utilizar el nombre de cada uno en las notificaciones)
+  const getUsers = () => {
+    // Solicitud GET (Request)
+    fetch(`${API_HOST}/?page=${currentPage}&results=20&seed=abc`)  // El query param seed es propio de la api que está utilizando, y permite generar el mismo conjunto de usuarios en cada llamado (por defecto la data es aleatoria)
+      // Exito
+      .then((response) => response.json()) // convertir a json
+      .then((json) => setUsers([...users, ...json.results])) // sumar los datos de la página actual al array de usuarios ya existente
+      .catch((err) => console.log("Solicitud fallida: ", err)); //  capturar errores
+  };
 
   const renderItem = ({ item }) => {
+    //Se aplican estilos particulares si el item está seleccionado (si el usuario lo presionó)
     const backgroundColor =
-      item.id === selectedItemId ? "teal" : "transparent";
+      item.id === selectedItemId ? "#009b97" : "transparent";
     const color = item.id === selectedItemId ? "white" : "black";
-    const iconColor = item.id === selectedItemId ? "#fdfdfd" : "teal";
+    const iconColor = item.id === selectedItemId ? "#fdfdfd" : "#009b97";
+
     return (
       <NotificationItem
-        notification={item}
+        user={item.name?.first}
         onPress={() => setSelectedItemId(item.id)}
-        backgroundColor={{ backgroundColor }}
-        textColor={{ color }}
-        iconColor={iconColor}
+        backgroundColor={{backgroundColor}}
+        textColor={{color}}
+        iconColor={{iconColor}}
       />
     );
   };
 
-  const renderLoader = () => (
-    <View>
-      <ActivityIndicator color="teal" style={styles.loader}/>
-    </View>
-  );
-
-  const renderSeparator = () => {
+  const renderLoader = () => {
     return (
-      <>
-        {Platform.OS !== "android" &&
-          (({ highlighted }) => (
-            <View
-              style={[styles.separator, highlighted && { marginLeft: 0 }]}
-            />
-          ))}
-      </>
+        <ActivityIndicator size="large" color="#009b97" style={styles.loader}  />
     );
   };
 
@@ -82,11 +94,22 @@ export default function NotificationsScreen() {
       <Text style={styles.title}>Notificaciones</Text>
       <FlatList
         style={styles.notificationsList}
-        data={notifications}
+        data={users}
         renderItem={renderItem}
-        keyExtractor={(item) => notifications.indexOf(item)}
-        ListFooterComponent={renderLoader}
-        ItemSeparatorComponent={renderSeparator}
+        keyExtractor={(item) => users.indexOf(item)}
+        removeClippedSubviews
+        ListFooterComponent={renderLoader} 
+        onEndReached={loadMoreItems} // Función que se va a ejecutar al llegar al final de la lista 
+        onEndReachedThreshold={0.2} // Permite que la función onEndReached se ejecute un momento antes de llegar al final de la lista, para evitar una "traba" cuando llega al final (mejora la experiencia). El número es la distancia que queda en el scroll para llegar el final del contenido
+        initialNumToRender={20} 
+        ItemSeparatorComponent={
+          Platform.OS !== "android" &&
+          (({ highlighted }) => (
+            <View
+              style={[styles.separator, highlighted && { marginLeft: 0 }]}
+            />
+          ))
+        }
       />
     </View>
   );
@@ -128,16 +151,16 @@ export default function NotificationsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "flex-start",
     marginTop: "10%",
   },
   title: {
     fontSize: 25,
     fontWeight: "500",
+    margin: "5%"
   },
   notificationsList: {
-    marginVertical: "5%",
+    paddingVertical: "2%",
   },
   notificationItem: {
     borderBottomColor: "grey",
@@ -152,6 +175,8 @@ const styles = StyleSheet.create({
     margin: "5%",
   },
   loader: {
-    marginVertical: "5%"
-  }
+    marginVertical: 10,
+    alignItems: "center",
+  },
 });
+
